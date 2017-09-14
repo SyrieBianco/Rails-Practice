5234f69ad02ac4e9a5965b2ff93e1d2e2258e0f9
Syrie Bianco <syriebianco@gmail.com>
2017-09-14 08:05:01 -0700

    Delete posts app from repo

--- a04_skeleton/config/routes.rb
+++
@@ -1,9 +0,0 @@
-PostsApp::Application.routes.draw do
-  root 'posts#index' 
-
-
-  resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
-  resources :posts
-  resources :tags, only: [:create, :destroy]
-end
