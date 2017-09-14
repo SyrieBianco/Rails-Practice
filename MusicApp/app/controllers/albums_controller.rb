5bde938b3791fde0cc4871ae505b829e82a84984
Syrie Bianco <syriebianco@gmail.com>
2017-09-14 09:23:22 -0700

    Complete controllers

--- MusicApp/app/controllers/albums_controller.rb
+++ MusicApp/app/controllers/albums_controller.rb
@@ -1,10 +1,49 @@
 class AlbumsController < ApplicationController
+
   def new
+    @band = Band.find(params[:band_id])
+    @album = @band.albums.new
   end
 
-  def edit
+  def create
+    @album = Album.new(album_params)
+
+    if @album.save
+      redirect_to album_url(@album)
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @album = Album.find(params[:id])
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    if @album.update_attributes(album_params)
+      redirect_to album_url(@album)
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+    redirect_to albums_url
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :year, :live, :band_id)
   end
 end
--- MusicApp/app/controllers/tracks_controller.rb
+++ MusicApp/app/controllers/tracks_controller.rb
@@ -1,10 +1,49 @@
 class TracksController < ApplicationController
+
   def new
+    @album = album.find(params[:album_id])
+    @track = @album.tracks.new
   end
 
-  def edit
+  def create
+    @track = Track.new(track_params)
+
+    if @track.save
+      redirect_to track_url(@track)
+    else
+      flash.now[:errors] = @track.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @track = Track.find(params[:id])
+  end
+
+  def edit
+    @track = Track.find(params[:id])
+  end
+
+  def update
+    @track = Track.find(params[:id])
+
+    if @track.update_attributes(track_params)
+      redirect_to track_url(@track)
+    else
+      flash.now[:errors] = @track.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @track = Track.find(params[:id])
+    @track.destroy
+    redirect_to tracks_url
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:title, :ord, :lyrics, :album_id, :bonus_track)
   end
 end
